138d6dedb6a5c455813ef9fa1c8a914db0e6b2c0
Shreya Mote <shreya.mote@nitorinfotech.com>
2026-03-11 12:44:37 +0530

    component creation

---
+++ frontend/meeting-ai-frontend/src/app/features/meeting-notes/components/meeting-notes/meeting-notes.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meeting-notes',
+  imports: [],
+  templateUrl: './meeting-notes.component.html',
+  styleUrl: './meeting-notes.component.css'
+})
+export class MeetingNotesComponent {
+
+}
